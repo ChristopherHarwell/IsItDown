df4257c2404f4afd67834fbe315bed7b443ca030
Christopher Harwell <christopherharwell@gmail.com>
2021-01-04 19:49:27 -0500

    Adds card component to display status

---
+++ frontend/isitdown/src/components/card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div>
+            <h4>{props.websiteName}</h4>
+            <hr />
+            <p>{props.statusCode}</p>
+            <p>{props.statusText}</p>
+            <p>{props.isDown}</p>
+            <p>{props.returnedUrl}</p>
+            <p>{props.requestedDomain}</p>
+        </div>
+    );
+};
+
+export default Card;
