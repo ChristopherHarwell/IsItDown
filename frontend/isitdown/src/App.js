c89ba35f353efec7294a3f686bfd3c84fcaace3f
Christopher Harwell <christopherharwell@gmail.com>
2021-01-07 00:59:54 -0500

    Adds dummy props to Card component

--- frontend/isitdown/src/App.js
+++ frontend/isitdown/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/card/Card';
+import Axios from 'axios';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Card statusCode={200} statusText={"OK"} isDown={false} returnedUrl={"https://www.someurl.com"} requestedDomain={"someurl"} ></Card>
     </div>
   );
 }
